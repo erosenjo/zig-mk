a9bb6064ed9efe05b02b63c006add4ea41664d8b
rosmck <124097135+rosmck@users.noreply.github.com>
2024-01-11 15:18:30 -0500

    notes

--- src/testLED.zig
+++ src/testLED.zig
@@ -1,8 +1,12 @@
 //this is where code for LED operation will go
 
+//use pio
+
 //function that outputs 0
 
 //function that outputs 1
+
+
 
 //function that converts desired color combo (hexcode) into 24bit int
 //NOTE: the order of the 24bit is GRB whereas hex is RGB
