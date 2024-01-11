e1da1df9604c93c98afc17fdfb413ac0a8f7dba8
rosmck <124097135+rosmck@users.noreply.github.com>
2024-01-11 15:07:50 -0500

    outline of needed functions

--- src/testLED.zig
+++ src/testLED.zig
@@ -1,1 +1,9 @@
 //this is where code for LED operation will go
+
+//function that outputs 0
+
+//function that outputs 1
+
+//function that converts desired color combo into 24bit int
+
+//output a 24bit
