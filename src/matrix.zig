1ee9bc5722dcaff4dc212ec5f145d59a2edc95b6
erosenjo <erosenjo@oberlin.edu>
2024-01-23 13:45:21 -0500

    Fixed the problems! It was pointers. and unmanaged memory...

--- src/matrix.zig
+++ src/matrix.zig
@@ -7,20 +7,16 @@ const Matrix = struct { .{ .{ 0, 0 }, .{ 0, 1 }, .{ 0, 2 } } };
 
 pub const USBBuffer = struct {
     mods: u8, // Modifier bitmap (is there a way to make this an enum?)
-    keys: *[14]u8, // Array of keycodes
+    keys: [14]u8, // Array of keycodes
 
     pub fn init() !USBBuffer {
         return USBBuffer{
             .mods = 0,
-            .keys = init: {
-                var arr: [14]u8 = undefined;
-                for (&arr) |*p| p.* = 0;
-                break :init arr[0..];
-            },
+            .keys = [_]u8{0} ** 14,
         };
     }
 
-    pub fn press(self: USBBuffer, keycode: u8) !void {
+    pub fn press(self: *USBBuffer, keycode: u8) !void {
         // while implementation
         //        var ctr: u8 = 0;
         //        while (self.keys[ctr] != 0) { // Goes through array looking for empty
@@ -30,7 +26,7 @@ pub const USBBuffer = struct {
         //        }
         //        self.keys[ctr] = keycode; // if empty is found, set it to key
         // for implementation
-        for (self.keys) |*key| {
+        for (&self.keys) |*key| {
             if (key.* == 0) { // if empty spot found, set it to KC and return
                 key.* = keycode;
                 return;
@@ -41,8 +37,7 @@ pub const USBBuffer = struct {
         return error.TooManyKeys;
     }
 
-    pub fn depress(self: USBBuffer, keycode: u8) !void {
-        _ = keycode;
+    pub fn depress(self: *USBBuffer, keycode: u8) !void {
         // while implementation
         // var ctr: u8 = 0;
         // while (self.keys[ctr] != key) { // Goes through array looking for key
@@ -50,23 +45,19 @@ pub const USBBuffer = struct {
         //     if (ctr >= 14) return error.KeyNotPressed;
         // }
         // self.keys[ctr] = 0; // if key is found, set it to empty
-        for (self.keys, 0..) |*key, i| {
-            std.debug.print("keycode at {d} is: {d}\n", .{ i, key.* });
-        }
         // for implementation
-        for (self.keys, 0..) |*key, i| {
+        for (&self.keys, 1..) |*key, i| {
             std.debug.print("keycode at {d} is: {d}\n", .{ i, key.* });
-            // if (key.* == keycode) {
-            //     var oldptr = key;
-            //     for (self.keys[(i + 1)..]) |*newptr| { //shift to fill gap
-            //         if (oldptr.* == 0) return;
-            //         oldptr.* = newptr.*;
-            //         oldptr = newptr;
-            //     }
-            //     self.keys[13] = 0;
-            //     return;
-            // }
-            // else if (key.* == 0) break;
+            if (key.* == keycode) {
+                var oldptr = key;
+                for (self.keys[i..]) |*newptr| { //shift to fill gap
+                    if (oldptr.* == 0) return;
+                    oldptr.* = newptr.*;
+                    oldptr = newptr;
+                }
+                self.keys[13] = 0;
+                return;
+            } else if (key.* == 0) break;
         }
         return error.KeyNotPressed;
     }
@@ -89,26 +80,8 @@ test "Press 1 button" {
 
 test "Press and release" {
     var buffer = try USBBuffer.init();
-
-    std.debug.print("Initial values: \n", .{});
-    for (buffer.keys, 0..) |*ptr, i| {
-        std.debug.print("Value at {d} is {d}.\n", .{ i, ptr.* });
-    }
-
     try buffer.press(@intFromEnum(KC.a));
-
-    std.debug.print("After press: \n", .{});
-    for (buffer.keys, 0..) |*ptr, i| {
-        std.debug.print("Value at {d} is {d}.\n", .{ i, ptr.* });
-    }
-
     try buffer.depress(@intFromEnum(KC.a));
-
-    std.debug.print("After depress: \n", .{});
-    for (buffer.keys, 0..) |*ptr, i| {
-        std.debug.print("Value at {d} is {d}.\n", .{ i, ptr.* });
-    }
-
     try testing.expect(buffer.keys[0] == 0);
 }
 
