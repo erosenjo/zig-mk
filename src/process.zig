dc1fe31d5101f2c8ec94f56d00e24e121e157aaf
rosmck <124097135+rosmck@users.noreply.github.com>
2024-01-25 16:10:33 -0500

    imported appropriate files, including temporarily the matrix.zig which has useful functions. May need to change the code function slightly to match type of KC. May rename code to translate to make more clear. Code function should index properly into the tuple used in example-keymap. Have not tested process but should work.

--- src/process.zig
+++ src/process.zig
@@ -9,16 +9,24 @@
 //};
 
 
-const numkeys: u8 = 4;
+//const numkeys: u8 = 4;
 
+const keymap = @import("example-keymap.zig");
+const buffer = @import("usb-buffer.zig");
+const funcaccess = @import("matrix.zig");
 
-fn translate(r: u8, c:u8) u8{
-	return(keymap.code(r, c));
+//example code
+fn code(r: u8, c:u8) u8{ //may need to return KC type instead
+	return(keymap[r][c]);
 }
 
+//fn translate(r: u8, c:u8) u8{
+//	return(keymap.code(r, c));
+//}
 
-pub fn process(comptime T: anytype, oldarray: [][]T , newarray: [][]T , buff: *USBBuffer) void{
-	var chg: [][]T = compare(oldarray, newarray);
+
+pub fn process(comptime T: anytype, oldarray: [][]T , newarray: [][]T , buff: *buffer.USBBuffer) void{
+	var chg: [][]T = funcaccess.compare(oldarray, newarray);
 	//check rows and columns of array, use press/depress as needed
 	var rowps: u8 = 0;
 	var colps: u8 = 0;
@@ -26,9 +34,9 @@ pub fn process(comptime T: anytype, oldarray: [][]T , newarray: [][]T , buff: *U
 	for (chg) |row|{
 		for (row) |col|{
 			if (col == 1){
-				buff.*.press(translate(rowps, colps)); //deref pointer to modify value
+				buff.*.press(code(rowps, colps)); //deref pointer to modify value
 			}else if(col == -1){
-				buff.*.depress(translate(rowps, colps)); //deref pointer to modify value
+				buff.*.depress(code(rowps, colps)); //deref pointer to modify value
 			}
 			colps += 1;
 		}
