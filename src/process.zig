c45f08a39c5b8d1500fdcfb1f23c23d925781440
erosenjo <erosenjo@oberlin.edu>
2024-01-26 13:33:55 -0500

    Integrated rows and columns into for loop for process().

--- src/process.zig
+++ src/process.zig
@@ -1,5 +1,3 @@
-
-
 //!Process takes old array, current array, usbbuffer uses/accesses keycode and keymap --> changes the usb buffer
 
 //import USB-buffer.zig
@@ -8,7 +6,6 @@
 //	placeholder : u8,
 //};
 
-
 //const numkeys: u8 = 4;
 
 const keymap = @import("example-keymap.zig");
@@ -16,32 +13,25 @@ const buffer = @import("usb-buffer.zig");
 const funcaccess = @import("matrix.zig");
 
 //example code
-fn code(r: u8, c:u8) u8{ //may need to return KC type instead
-	return(keymap[r][c]);
+fn code(r: u8, c: u8) u8 { //may need to return KC type instead
+    return (keymap[r][c]);
 }
 
 //fn translate(r: u8, c:u8) u8{
 //	return(keymap.code(r, c));
 //}
 
+pub fn process(comptime T: anytype, oldarray: [][]T, newarray: [][]T, buff: *buffer.USBBuffer) void {
+    var chg: [][]T = funcaccess.compare(oldarray, newarray);
+    //check rows and columns of array, use press/depress as needed
 
-pub fn process(comptime T: anytype, oldarray: [][]T , newarray: [][]T , buff: *buffer.USBBuffer) void{
-	var chg: [][]T = funcaccess.compare(oldarray, newarray);
-	//check rows and columns of array, use press/depress as needed
-	var rowps: u8 = 0;
-	var colps: u8 = 0;
-
-	for (chg) |row|{
-		for (row) |col|{
-			if (col == 1){
-				buff.*.press(code(rowps, colps)); //deref pointer to modify value
-			}else if(col == -1){
-				buff.*.depress(code(rowps, colps)); //deref pointer to modify value
-			}
-			colps += 1;
-		}
-		rowps += 1;
-		colps = 0;
-	}
-
+    for (chg, 0..) |row, rowps| {
+        for (row, 0..) |col, colps| {
+            if (col == 1) {
+                buff.*.press(code(rowps, colps)); //deref pointer to modify value
+            } else if (col == -1) {
+                buff.*.depress(code(rowps, colps)); //deref pointer to modify value
+            }
+        }
+    }
 }
