667e02d792aba40d3da6b68ec7508148f42230c6
rosmck <124097135+rosmck@users.noreply.github.com>
2024-01-26 15:06:24 -0500

    main outline

--- src/main.zig
+++ src/main.zig
@@ -8,3 +8,24 @@
 //process
 
 //provide USBbuffer
+
+const b = @import("usb-buffer.zig");
+const p = @import("process.zig");
+const mkchg = @import("matrix.zig").makeChangeArray;
+
+fn scan() [][]u8 {
+    return ([_][1]u8{[_]u8{ 1, 0, 1, 0 }});
+}
+
+pub fn main() !void {
+    var buffer: b.USBBuffer = b.USBBuffer.init();
+
+    var newarray = mkchg();
+    var oldarray = mkchg();
+
+    while (true) {
+        oldarray = newarray; //old array pointer points to new array
+        newarray = scan(); //new array pointer points to scanned array
+        p.process(u8, oldarray, newarray, buffer);
+    }
+}
